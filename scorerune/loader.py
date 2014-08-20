ec29a4ec80eeb4c2385d9df12e1020ae7fe0f222
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2014-08-20 17:39:00 +0700

    style: clean up csharp runtime

--- scorerune/loader.py
+++ scorerune/loader.py
@@ -5,3 +5,10 @@ validates structure eagerly and raises LoadError with an aggregated, readable
 message rather than letting a malformed rubric surface later as a scoring bug.
 """
 
+from __future__ import annotations
+
+import json
+from pathlib import Path
+from typing import Any
+
+from .model import Candidate, Rubric
