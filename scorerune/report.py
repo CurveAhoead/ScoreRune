0612efc556553f8d16d611bbb2e3b54b920c0728
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2014-09-23 18:28:00 +0700

    feat: add weighted criteria

--- scorerune/report.py
+++ scorerune/report.py
@@ -4,3 +4,9 @@ The reporters are pure string producers so callers decide where output goes.
 JSON output uses sorted keys and a fixed indent to keep results diff-stable.
 """
 
+from __future__ import annotations
+
+import json
+
+from .model import Ranking, Scorecard
+
