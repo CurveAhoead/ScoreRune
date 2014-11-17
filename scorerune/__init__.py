2889b0107e5d9a944bb3ac3e161ed415e1497197
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2014-11-17 16:15:00 +0700

    docs: expand rubric scoring

--- scorerune/__init__.py
+++ scorerune/__init__.py
@@ -2,3 +2,9 @@
 
 Public API surface:
 
+    from scorerune import (
+        Rubric, Criterion, Candidate, Scorecard, Ranking,
+        score_candidate, rank_candidates,
+        load_rubric, load_candidates,
+    )
+"""
