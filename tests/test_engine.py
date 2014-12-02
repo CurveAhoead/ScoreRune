3518eb73f3e9f0eabf09626fcab5c6c869f7622b
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2014-12-02 17:15:00 +0700

    chore: bump rubric loader

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -7,3 +7,8 @@ from pathlib import Path
 from scorerune.engine import score_candidate
 from scorerune.loader import load_candidates, load_rubric
 
+EX = Path(__file__).resolve().parent.parent / "examples"
+
+
+class EngineTests(unittest.TestCase):
+    @classmethod
