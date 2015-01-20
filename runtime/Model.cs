1abe833d2be6a23387fcbadf555e3757a5d2dc1c
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2015-01-20 14:12:00 +0700

    fix: correct json report

--- runtime/Model.cs
+++ runtime/Model.cs
@@ -4,3 +4,10 @@ namespace ScoreRune.Runtime;
 
 /// <summary>
 /// Value types describing rubrics, candidates, and scoring results. These mirror
+/// the Python <c>scorerune.model</c> structures so scorecards produced by either
+/// runtime carry the same fields and semantics.
+/// </summary>
+public sealed record Criterion
+{
+    [JsonPropertyName("id")] public string Id { get; init; } = "";
+    [JsonPropertyName("title")] public string Title { get; init; } = "";
