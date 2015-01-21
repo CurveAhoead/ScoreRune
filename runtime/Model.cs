394974e7851a114cf218a5c60743d9ad55a77ef2
CurveAhoead <894295+CurveAhoead@users.noreply.github.com>
2015-01-21 17:34:00 +0700

    feat: tie-break

--- runtime/Model.cs
+++ runtime/Model.cs
@@ -11,3 +11,10 @@ public sealed record Criterion
 {
     [JsonPropertyName("id")] public string Id { get; init; } = "";
     [JsonPropertyName("title")] public string Title { get; init; } = "";
+    [JsonPropertyName("weight")] public double Weight { get; init; } = 1.0;
+    [JsonPropertyName("kind")] public string Kind { get; init; } = "phrase";
+    [JsonPropertyName("params")] public Dictionary<string, object> Params { get; init; } = new();
+    [JsonPropertyName("description")] public string Description { get; init; } = "";
+}
+
+public sealed record Rubric
